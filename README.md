9ec7d288e608009dcde2f6574da260079dd78c9b
Riim <riim@yandex.ru>
2017-01-02 02:16:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# escape-string
+
+Escape string special characters.
+
+## Install
+
+```
+npm install escape-string --save
+```
+
+## Usage
+
+```js
+import escapeString from 'escape-string';
+// or `var escapeString = require('escape-string').default;`
+
+escapeString(` \\ ' " \r \n `);
+// => ' \\\\ \\' \\" \\r \\n '
+
+Function(`return '${ escapeString(` \\ ' " \n \r `) }';`)();
+// => ' \\ ' " \r \n '
+```
---
+++ src/escapeString.ts
@@ -0,0 +1,12 @@
+let reEscapableChars = /[\\'"\r\n]/g;
+let charToSpecialMap = Object.create(null);
+
+charToSpecialMap['\\'] = '\\\\';
+charToSpecialMap['\''] = '\\\'';
+charToSpecialMap['"'] = '\\"';
+charToSpecialMap['\r'] = '\\r';
+charToSpecialMap['\n'] = '\\n';
+
+export default function escapeString(str: string): string {
+	return reEscapableChars.test(str) ? str.replace(reEscapableChars, chr => charToSpecialMap[chr]) : str;
+}
